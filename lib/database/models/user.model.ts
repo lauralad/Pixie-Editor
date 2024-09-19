89d778511690190af9355b51ad8640e8f0ed2a45
lauralad <98854129+lauralad@users.noreply.github.com>
2024-09-19 16:31:59 -0400

    adding clerk webhooks

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -4,16 +4,54 @@
 import { model, models, Schema } from "mongoose";
 
 
+// const UserSchema = new Schema({
+//     clerkId: {type: String, required: true, unique: true},
+//     email: {type: String, required: true, unique: true},
+//     username: {type: String, required: true, unique: true},
+//     photo: {type: Object, required: true},
+//     firstName: {type: String, required: false},
+//     lastName: {type: String, required: false},
+//     planId: {type: Number, default: 1},
+//     creditBalance: {type: Number, default: 10}
+// });
+
 const UserSchema = new Schema({
-    clerkId: {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    username: {type: String, required: true, unique: true},
-    photo: {type: Object, required: true},
-    firstname: {type: String, required: false},
-    lastname: {type: String, required: false},
-    planId: {type: Number, default: 1},
-    creditBalance: {type: Number, default: 10}
-});
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: false,
+    },
+    lastName: {
+      type: String,
+      required: false,
+    },
+    planId: {
+      type: Number,
+      default: 1,
+    },
+    creditBalance: {
+      type: Number,
+      default: 10,
+    },
+  });
 
 const User = models?.User || model('User', UserSchema);
 
